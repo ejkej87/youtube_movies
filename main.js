bff9325a7351a36596048d707f426ce44636faf2
Arkadiusz Garzel <ejkej87@gmail.com>
2022-10-18 11:00:21 +0200

    add logic id or link yotutube

--- main.js
+++ main.js
@@ -1,82 +1,21 @@
-const input = document.querySelector('#videoID')
-const btn = document.querySelector('#btn')
+let input = document.querySelector('input[type="text"]');
+let button = document.querySelector('input[type="button"]');
+let output = document.querySelector('output');
+let figure = document.querySelector('figure');
 
-let videoInitialized = false
+function youtubeUrlParser(url) {
+    let videoId = /^https?\:\/\/(www\.)?youtu\.be/.test(url) ? url.replace(/^https?\:\/\/(www\.)?youtu\.be\/([\w-]{11}).*/, "$2") : url.replace(/.*\?v\=([\w-]{11}).*/, "$1");
+    return {
+        id: videoId
+    };
 
-btn.addEventListener('click', () => {
-
-    if (videoInitialized) {
-        console.log(2)
-        player.loadVideoById(input.value, 0)
-    } else {
-        initializeVideo()
-        console.log(1)
-    }
-});
-
-// sample id: 'M7lc1UVf-VE'
-function initializeVideo() {
-    console.log(4)
-    videoInitialized = true
-
-    let tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    let firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-}
-
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-let done = false;
-
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-
-function onPlayerError(event) {
-    // alert('Podaj prawidłowy ID filmu')
-    console.log('Error: ' + event.data)
-}
-
-let player;
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '360',
-        width: '640',
-        videoId: input.value,
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange,
-            'onError': onPlayerError
-        }
-    });
-}
+}; // youtubeParser();
 
 
-function stopVideo() {
-    player.stopVideo();
-}
+let outputResult = function () {
+    let video = youtubeUrlParser(input.value);
+    figure.innerHTML = '<iframe src="https://www.youtube.com/embed/' + video.id + '?' + (video.startSeconds ? 'start=' + video.startSeconds + '&amp;' : '') + 'enablejsapi=1&amp;autohide=1&amp;color=white&amp;controls=1&amp;playsinline=1&amp;rel=' + video.showRelated + '&amp;autoplay=true&amp;showinfo=0&amp;theme=light&amp;wmode=transparent" width="900" height="600" allowfullscreen></iframe>';
+    output.textContent = 'ID: ' + video.id + (video.startString ? ' | Start @ ' + video.startString.replace(/t\=/, '').replace(/[a-z]/g, ':').replace(/\:$/, '') : '');
+};
 
-// M7lc1UVf-VE
-
-// regex 
-// function youtube_parser(url) {
-//     let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-//     let match = url.match(regExp);
-//     if (match && match[7].length == 11) {
-//         let b = match[7];
-//         alert(b);
-//         console.log(b)
-//     } else {
-//         alert("Url jest błedny");
-//     }
-// }
-
-// const checkURL = youtube_parser(input.value)
+button.addEventListener('click', outputResult);
